9a3c8a984e4e22d82056778abd45d944bd3d9e54
NgTrNhatVu <61381778+NgTrNhatVu@users.noreply.github.com>
2022-09-01 15:05:23 +0700

    Thêm bảng ds sinh viên nghỉ nhiều

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,37 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Classes;
 use App\Models\Course;
 use App\Models\Student;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class AdminController extends Controller
 {
     public function index()
     {
         $courses = Course::where([
-            ['scheduled_day', '!=', null],
-            ['scheduled_time', '!=', null],
-            ['status', '=', 1]]
+                ['scheduled_day', '!=', null],
+                ['scheduled_time', '!=', null],
+                ['status', '=', 1]]
         )->get();
         foreach ($courses as $course) {
-            $course->scheduled_time = explode(' - ',$course->scheduled_time);
+            $course->scheduled_time = explode(' - ', $course->scheduled_time);
             $course->start = $course->scheduled_time[0];
             $course->end = $course->scheduled_time[1];
-            $course->total_lessons = $course->total_hours/(count($course->scheduled_time));
+            $course->total_lessons = $course->total_hours / (count($course->scheduled_time));
         }
         return view('admin.index', ['courses' => $courses]);
     }
 
-    public function statistic(){
+    public function statistic()
+    {
+        // Thống kê chung
         $lecturerQuan = User::where('role', 0)->count();
         $courseQuan = Course::where('status', 1)->count();
         $studentQuan = Student::where('status', 1)->count();
+        $classQuan = Classes::where('status', 1)->count();
+
+        $attendanceAbsents = self::getStudentAbsentTooMuch(15, 1, 0);
+
         return view('admin.statistic.index', [
             'lecturerQuan' => $lecturerQuan,
             'courseQuan' => $courseQuan,
-            'studentQuan' => $studentQuan
+            'studentQuan' => $studentQuan,
+            'classQuan' => $classQuan,
+            'attendanceNoReason' => $attendanceAbsents
         ]);
+    }
+
+    /*
+     * Lấy danh sách sinh viên nghỉ quá nhiều
+     * $duration: Khoảng thời gian bắt đầu tính (vd: 14 -> 14 ngày gần nhất)
+     * times: Số lần vi phạm tối đa
+     * $status: Trạng thái nghỉ (0 - Nghỉ, 1 - Muộn, 2 - Nghỉ có phép
+     */
+    private function getStudentAbsentTooMuch($duration, $times, $status){
+        $statusConvert = ["without reason", "late", "with reason"];
+        $date = Carbon::now()->subDays($duration);
+
+        $results = Student::selectRaw('student.id, student.full_name, student.code,
+            student.class_id, student.birthdate, attendance.attendant_status,
+            count(attendance.attendant_status) as count')
+            ->leftJoin('attendance', 'student.id', '=', 'attendance.student_id')
+            ->where("attendance.created_at", '>=', $date)
+            ->groupBy('student.id', 'attendance.attendant_status')
+            ->having('attendance.attendant_status', $statusConvert[$status])
+            ->get();
+
+        $count = count($results);
+        for ($i = 0; $i < $count; $i++) {
+            if ($results[$i]->count <= $times) {
+                $results->forget($i);
+            } else{
+                $results[$i]->class_name =
+                    Classes::select('name')
+                        ->where('id', $results[$i]->class_id)
+                        ->pluck('name')->first();
+            }
+        }
+        return $results;
     }
 }
--- resources/views/admin/statistic/index.blade.php
+++ resources/views/admin/statistic/index.blade.php
@@ -1,20 +1,21 @@
 @extends('admin.layout.main')
 @section('title', 'Lớp học')
 @section('content')
+    <h2 class="fw-bold mb-5 text-center">THỐNG KÊ CHUNG</h2>
     @isset($lecturerQuan)
-        <div class="row">
+        <div class="row justify-content-center">
             <div class="col-lg-3 col-6">
                 <!-- small card -->
                 <div class="small-box bg-info">
                     <div class="inner">
                         <h3>{{$courseQuan}}</h3>
 
-                        <p>Lớp môn học</p>
+                        <p>Lớp môn học chưa kết thúc</p>
                     </div>
                     <div class="icon">
-                        <i class="fas fa-school"></i>
+                        <i class="fas fa-chalkboard"></i>
                     </div>
-                    <a href="#" class="small-box-footer">
+                    <a href="{{ asset('admin/course') }}" class="small-box-footer">
                         Chi tiết <i class="fas fa-arrow-circle-right"></i>
                     </a>
                 </div>
@@ -28,7 +29,7 @@
                         <p>Giảng viên</p>
                     </div>
                     <div class="icon">
-                        <i class="fas fa-user-alt"></i>
+                        <i class="fas fa-glasses"></i>
                     </div>
                     <a href="#" class="small-box-footer">
                         Chi tiết <i class="fas fa-arrow-circle-right"></i>
@@ -44,12 +45,75 @@
                         <p>Sinh viên đang theo học</p>
                     </div>
                     <div class="icon">
-                        <i class="fa fa-users" aria-hidden="true"></i>
+                        <i class="fa fa-user-graduate" aria-hidden="true"></i>
                     </div>
-                    <a href="#" class="small-box-footer">
+                    <a href="{{ asset('admin/student') }}" class="small-box-footer">
                         Chi tiết <i class="fas fa-arrow-circle-right"></i>
                     </a>
                 </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <!-- small card -->
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{$classQuan}}</h3>
+
+                        <p>Lớp học</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-users" aria-hidden="true"></i>
+                    </div>
+                    <a href="{{ asset('admin/class') }}" class="small-box-footer">
+                        Chi tiết <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        {{--Sinh viên nghỉ học nhiều--}}
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-danger collapsed-card">
+                    <div class="card-header">
+                        <h3 class="card-title">Sinh viên nghỉ học nhiều 30 ngày gần đây</h3>
+
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-plus"></i>
+                            </button>
+                        </div>
+                        <!-- /.card-tools -->
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="datatable" class="table align-middle table-bordered">
+                            <thead>
+                            <tr class="bg-white">
+                                <th class="border-0 text-center">#</th>
+                                <th class="border-0 text-center">Tên sinh viên</th>
+                                <th class="border-0 text-center">Mã sinh viên</th>
+                                <th class="border-0 text-center">Ngày sinh</th>
+                                <th class="border-0 text-center">Lớp</th>
+                                <th class="border-0 text-center">Số buổi nghỉ 30 ngày gần đây</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($attendanceNoReason as $each)
+                                <tr>
+                                    <td>{{$loop->index+1}}</td>
+                                    <td>{{$each->full_name}}</td>
+                                    <td>{{$each->code}}</td>
+                                    <td>{{$each->birthdate}}</td>
+                                    <td>{{$each->class_name}}</td>
+                                    <td>{{$each->count}}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
             </div>
         </div>
     @endisset
