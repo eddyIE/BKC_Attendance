c397845b0e0e709c0da603deee656fbcb06d5a41
NgTrNhatVu <61381778+NgTrNhatVu@users.noreply.github.com>
2022-09-04 21:19:28 +0700

    Update chọn khóa học

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -41,26 +41,4 @@ class Course extends Model
     {
         return $this->belongsTo(ProgramInfo::class, 'subject_id', 'id');
     }
-
-    /*
-     * Tìm khóa học được phân công của giảng viên
-     */
-    public function findCoursesOfLecturer($userId, $onlyUnfinishedCourse = true)
-    {
-        if (!$onlyUnfinishedCourse) {
-            return Course::select('course.*')
-                ->join('lecturer_scheduling', 'course.id', '=',
-                    'lecturer_scheduling.course_id')
-                ->join('user', 'user.id', '=', 'lecturer_scheduling.lecturer_id')
-                ->where(['lecturer_scheduling.lecturer_id' => $userId])
-                ->get();
-        }
-        return Course::select('course.*')
-            ->join('lecturer_scheduling', 'course.id', '=',
-                'lecturer_scheduling.course_id')
-            ->join('user', 'user.id', '=', 'lecturer_scheduling.lecturer_id')
-            ->where(['course.status' => 1,
-                'lecturer_scheduling.lecturer_id' => $userId])
-            ->get();
-    }
 }
